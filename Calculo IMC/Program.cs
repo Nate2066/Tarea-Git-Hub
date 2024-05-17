7f0828d89533530a2872705ada80a991c028d8c7
Nate2066 <fredericknat1522@gmail.com>
2024-05-16 19:25:28 -0500

    Agregar archivos de proyecto.

---
+++ Calculo IMC/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ConvertorIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite su peso (Kilogramos)");
+                Console.Write(">>");
+                double Peso = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite su altura (Metros)");
+                Console.Write(">>");
+                double Altura = Convert.ToDouble(Console.ReadLine());
+
+                double IMC = Peso / (Altura * Altura);
+
+                Console.WriteLine("Su Indice de masa corporal es de : " + IMC);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error! Valor invalido");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error! Valor demasiado grande");
+            }
+        }
+    }
+}
+
